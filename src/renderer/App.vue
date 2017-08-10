fc212c85519aa34a7a2c3d1076564a1c6d8c177e
iLan Epstein <iLan@mentalfabrications.com>
2017-08-10 16:04:47 -0700

    Initial boilerplate

---
+++ src/renderer/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <article class="container">
+      <div class="page-container">
+        <!-- <router-view></router-view> -->
+        <object data="http://www.dairiki.org/tides/monthly.php/sea" type="text/html" style="width:100%; height:100%;"></object>
+      </div>
+      <div class="controls-container">
+        <p>Some controls here!</p>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'scrapifier'
+  }
+</script>
+
+<style lang="scss">
+  article.container {
+    display: flex;
+
+    .page-container {
+      flex: 3 0 auto;
+    }
+
+    .controls-container {
+      flex: 1 0 auto;
+    }
+  }
+</style>
