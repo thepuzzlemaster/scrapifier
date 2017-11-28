dfe795f505722a159c31f75d8d09e5a04e251dd3
iLan Epstein <iLan@mentalfabrications.com>
2017-11-27 16:53:36 -0800

    Migrate WebContent.js into component

--- src/renderer/components/WebContent.vue
+++ src/renderer/components/WebContent.vue
@@ -4,7 +4,7 @@
 
 <script>
   import cheerio from 'cheerio'
-  import WebContent from './WebContent/webContent'
+  import $ from 'jquery'
 
   var existingHeadHtml = ''
 
@@ -16,7 +16,8 @@
     },
     data: function () {
       return {
-        html: '<p>Loading...</p>'
+        html: '<p>Loading...</p>',
+        currentSelector: ''
       }
     },
     watch: {
@@ -27,9 +28,105 @@
       }
     },
     methods: {
-      startScraping: function (selector) {
-        console.log('WebContent.startScraping', selector)
-        WebContent.startScraping(selector)
+      startScraping: function (incomingSelector) {
+        console.log('WebContent.startScraping', incomingSelector)
+        this.currentSelector = incomingSelector || ''
+        this.addHighlight(null, null, this.currentSelector)
+        document.addEventListener('mousemove', this.moveHandler)
+      },
+
+      //
+      // HIGHLIGHT METHODS
+      addHighlight: function ($element, moveEvent, selector, showAppend) {
+        function removeHighlight () {
+          $(this).removeClass('selector-hover')
+          $(this).removeClass('hover-primary')
+          $(this).removeClass('hover-secondary')
+          $(this).off('click')
+        }
+        const selectorIsEmpty = selector === ''
+        selector = selectorIsEmpty ? '' : selector || this.getSelector($(moveEvent.target))
+
+        // Remove existing hovers
+        $('.selector-hover').each(removeHighlight)
+
+        if ($element) {
+          $element.addClass('selector-hover hover-primary')
+          $element.removeClass('hover-secondary')
+          $element.on('click', (event) => {
+            document.removeEventListener('mousemove', this.moveHandler)
+            event.preventDefault()
+            $element.removeClass('hover-primary')
+            this.$emit('selectorClick', {
+              selector: selector
+            })
+          })
+        }
+
+        $(selector).addClass('selector-hover hover-secondary')
+        // ipcRenderer.send('selector-info', {
+        //   count: $(selector).length,
+        //   selector: selector,
+        //   showAppend: showAppend
+        // })
+      },
+
+      //
+      // EVENT HANDLERS
+      moveHandler: function (event) {
+        const x = event.clientX
+        const y = event.clientY
+        var element = document.elementFromPoint(x, y)
+
+        this.addHighlight($(element), event)
+      },
+
+      //
+      // HELPER METHODS
+      getSelector: function ($el) {
+        function getTag ($el) {
+          return $el.get(0).tagName.toLowerCase()
+        }
+
+        function getClasses ($el) {
+          let classes = ''
+          function stripHoverClasses (classes) {
+            return classes.replace('.selector-hover', '')
+              .replace('.hover-primary', '')
+              .replace('.hover-secondary', '')
+          }
+
+          if ($el.attr('class')) {
+            classes = '.' + $el.attr('class').split(' ').join('.')
+            classes = stripHoverClasses(classes)
+          }
+
+          return classes
+        }
+
+        function getSelectorString ($el) {
+          const tag = getTag($el)
+          const classes = getClasses($el)
+
+          if (tag !== 'html' && tag !== 'body') {
+            return tag + classes
+          }
+
+          return ''
+        }
+
+        function stripWhitespace (selector) {
+          // Strip out leading and trailing commas and spaces
+          selector = selector.replace(/^,\s*|,\s*$/, '')
+          return selector
+        }
+
+        const $parent = $el.parent()
+        const elSelector = getSelectorString($el)
+        const parentSelector = getSelectorString($parent)
+
+        var returnSelector = `${this.currentSelector}, ${parentSelector} ${elSelector}`
+        return stripWhitespace(returnSelector)
       }
     },
     mounted: function () {
