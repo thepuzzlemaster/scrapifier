bfd2a15e69b575cdf160208c74602a32d2ac616c
iLan Epstein <iLan@mentalfabrications.com>
2018-01-02 14:07:48 -0800

    App.vue no longer needs to proxy components

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,14 +5,8 @@
         <!-- <object id="external-page" :data="url" type="text/html" style="width:100%; height:100%;"></object> -->
       <!-- </div> -->
       <!-- <router-view :url="url"></router-view> -->
-      <web-content :url="url"
-                   v-on:selectorInfo="selectorInfo"
-                   v-on:selectorClick="selectorClick"></web-content>
-      <controls class="controls" :count="count"
-                                 :show-append="showAppend"
-                                 :url="url"
-                                 v-on:selectorChange="setSelector"
-                                 v-on:isScraping="startScraping"></controls>
+      <web-content :url="url"></web-content>
+      <controls class="controls" :url="url"></controls>
     </div>
   </div>
 </template>
@@ -31,38 +25,12 @@
     data: function () {
       return {
         url: 'http://localhost:8889/www.dairiki.org/tides/monthly.php/sea',
-        count: 0,
-        selector: '',
         showAppend: false
       }
     },
     computed: mapState({
-      scraping: state => state.ScrapingData.scraping
     }),
     methods: {
-      startScraping: function (isScraping, options) {
-        console.log('App.startScraping', isScraping, options)
-        var scrapingMode = options && options.mode
-        var selector = options && options.selector
-        this.$store.commit('setScraping', isScraping)
-        this.$store.commit('setScrapingMode', scrapingMode)
-        this.$store.commit('setSelector', selector)
-        this.showAppend = !isScraping
-      },
-      selectorInfo: function (options) {
-        this.count = options.count
-        this.scraping.selector = options.selector
-        this.showAppend = options.showAppend
-      },
-      selectorClick: function (options) {
-        this.showAppend = true
-        this.scraping.isScraping = false
-        this.scraping.selector = options && options.selector
-      },
-      setSelector: function (options) {
-        this.showAppend = true
-        this.scraping.selector = options.selector
-      }
     },
     mounted: function () {
       // axios.get(this.url).then((response) => {
